50d89ed929d4595500be2779cf5c0f3803232814
Mariusz Zawadzki <mariusz.r.zawadzki@gmail.com>
2018-02-26 20:07:00 +0100

    add webflux and a sample endpoint

--- src/main/kotlin/pl/monku/weathersaver/WeatherSaverApplication.kt
+++ src/main/kotlin/pl/monku/weathersaver/WeatherSaverApplication.kt
@@ -2,10 +2,21 @@ package pl.monku.weathersaver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
 
 @SpringBootApplication
-class WeatherSaverApplication
+open class WeatherSaverApplication
 
 fun main(args: Array<String>) {
     runApplication<WeatherSaverApplication>(*args)
+}
+
+
+@RestController
+open class TestController {
+
+    @GetMapping("/test")
+    fun testEndpoint() = Mono.just("Test mono")
 }
